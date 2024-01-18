054b16bb8637195206aaf6b8a19494e0dd38b593
Shmagi97 <135329382+Shmagi97@users.noreply.github.com>
2024-01-18 14:58:03 +0400

    registre no end

--- next-pf/app/components/logginRegisterModal/register.tsx
+++ next-pf/app/components/logginRegisterModal/register.tsx
@@ -16,77 +16,94 @@ export const Register = () => {
 
                <label htmlFor="forLabel1">სახელი</label>
                <input type="text" id='forLabel1'/>
-               <span></span>
-               <span></span>
+               <article>
+                 <span></span>
+                 <span></span>
+               </article>
 
             </div>
             <div  className={style.div2}>
                
                <label htmlFor="forLabel2">გვარი</label>
                <input type="text" id='forLabel2'/>
-               <span></span>
-               <span></span>
+               <article>
+                 <span></span>
+                 <span></span>
+               </article>
 
             </div>
             <div  className={style.div3}>
                  
                <label htmlFor="forLabel3">მომხმარებელი</label>
                <input type="text" id='forLabel3'/>
+               <article>
                  <span></span>
                  <span></span>
                  <span></span>
                  <span></span>
+               </article>
+                
 
             </div>
             <div  className={style.div4}>
             
                <label htmlFor="forLabel4">ელ-ფოსტა</label>
                <input type="text" id='forLabel4'/>
-                <span></span>
-                <span></span>
-                <span></span>
+               <article>
+                 <span></span>
+                 <span></span>
+                 <span></span>
+               </article>
 
             </div>
             <div  className={style.div5}>
 
                <label htmlFor="forLabel5">პაროლი</label>
                <input type="text" id='forLabel5'/>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
+               <article>
+                 <span></span>
+                 <span></span>
+                 <span></span>
+                 <span></span>
+               </article>
 
             </div>
             <div  className={style.div6}>
 
                 <label htmlFor="forLabel6">გაიმეორეთ პაროლი</label>
                 <input type="text" id='forLabel6'/>
+                <article>
                  <span></span>
                  <span></span>
                  <span></span>
                  <span></span>
+               </article>
 
             </div>
             <article  className={ `${style.article} ${style.di7Agaim}` }>
 
                 <label htmlFor="fromLabel7">
                  
-                   <input type='checkbox' id='fromLabel7'/>
-                   <span></span>
+                   <input className={style.checkbox} type='checkbox' id='fromLabel7'/>
+                   <span className={style.checkboxSpan}></span>
 
                 </label>
 
-             <div>
+             <div className={style.textDiv}>
                 <p>
                 ღილაკზე „რეგისტრაცია“ დაწკაპუნებით, თქვენ დაეთანხმებით ჩვენს
                 </p>
-                <Link href={'/'}>მოხმარების წესები</Link>
+                <div className={style.linkDiv}>
+                <Link className={style.linkTeg} href={'/'}>მოხმარების წესები</Link>
                  <span>"&"</span>
-                 <Link href={'/'}>კონფიდენციალობის პოლიტიკა</Link>
+                 <Link className={style.linkTeg} href={'/'}>კონფიდენციალობის პოლიტიკა</Link>
+                 </div>
              </div>    
                  
             </article>
-             <button type='submit'>რეგისტრაცია</button>
+            
+                <button type='submit' className={style.buttonSubmit}>რეგისტრაცია</button>
+         
          </form>
 
     </section>
