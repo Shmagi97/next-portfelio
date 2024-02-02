ac27e60e2d566badcad24f1f30291f39bbe0bed2
Shmagi97 <135329382+Shmagi97@users.noreply.github.com>
2024-02-03 00:37:20 +0400

    update npm and latest nextjs

--- next-pf/app/components/footerAndHeader/footer/footer.tsx
+++ next-pf/app/components/footerAndHeader/footer/footer.tsx
@@ -1,13 +1,19 @@
 'use client'
 
 
-import style from './footer.module.css'
+import style from './footer.module.scss'
 
 const Footer = () => {
     return <footer className={style.footer}>
     
       <section className={style.footerSection}>
-           <p>არ შეიცავს ინფორმაციას</p>
+           <div>
+            <span> Web Development `Next.js and TypeScript` </span>
+           </div>
+           <div>
+             <span>შმაგი ნარსავიძე</span>
+             <span>568 59 60 12</span>
+           </div>
       </section>
     
     </footer>
